bcea5e09134c202746565746582968600b709f0b
rlawo <rlawo32@naver.com>
2024-12-08 23:03:20 +0900

    First

---
+++ src/main/java/riot/lcgs/riotlcgsbe/RiotLcgsBeApplication.java
@@ -0,0 +1,13 @@
+package riot.lcgs.riotlcgsbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiotLcgsBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RiotLcgsBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/riot/lcgs/riotlcgsbe/RiotLcgsBeApplicationTests.java
@@ -0,0 +1,13 @@
+package riot.lcgs.riotlcgsbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RiotLcgsBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
