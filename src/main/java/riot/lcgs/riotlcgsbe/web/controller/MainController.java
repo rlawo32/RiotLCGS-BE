461b065436e72b23d89e6a7c96ef2ab6aba807c6
rlawo <rlawo32@naver.com>
2024-12-08 23:12:45 +0900

    [RiotLCGS][BE] <Controller> Main_작업_20241208 (1)

---
+++ src/main/java/riot/lcgs/riotlcgsbe/web/controller/MainController.java
@@ -0,0 +1,19 @@
+package riot.lcgs.riotlcgsbe.web.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import riot.lcgs.riotlcgsbe.web.dto.CustomGameRequestDto;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/riot")
+public class MainController {
+
+    @PostMapping("/insertData")
+    public String insertData(@RequestBody CustomGameRequestDto requestDto) {
+        return "";
+    }
+}
