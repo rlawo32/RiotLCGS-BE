ea6a495a5602f2075f6ca2e703a4dad2bd23fb45
rlawo <rlawo32@naver.com>
2024-12-09 22:58:27 +0900

    [RiotLCGS][BE] <Dto> DataObject_작업_20241209 (6)

---
+++ src/main/java/riot/lcgs/riotlcgsbe/web/dto/object/TeamData.java
@@ -0,0 +1,17 @@
+package riot.lcgs.riotlcgsbe.web.dto.object;
+
+import lombok.Data;
+
+@Data
+public class TeamData {
+    private String teamA1;
+    private String teamA2;
+    private String teamA3;
+    private String teamA4;
+    private String teamA5;
+    private String teamB1;
+    private String teamB2;
+    private String teamB3;
+    private String teamB4;
+    private String teamB5;
+}
