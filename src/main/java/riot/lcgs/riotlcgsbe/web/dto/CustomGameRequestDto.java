0e974c0613bb397519e6a523a2a613b6e5770fe8
rlawo <rlawo32@naver.com>
2024-12-09 22:58:56 +0900

    [RiotLCGS][BE] <Dto> GameData_작업_20241209 (7)

--- src/main/java/riot/lcgs/riotlcgsbe/web/dto/CustomGameRequestDto.java
+++ src/main/java/riot/lcgs/riotlcgsbe/web/dto/CustomGameRequestDto.java
@@ -3,12 +3,14 @@ package riot.lcgs.riotlcgsbe.web.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import riot.lcgs.riotlcgsbe.web.dto.object.GameData;
+import riot.lcgs.riotlcgsbe.web.dto.object.TeamData;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class CustomGameRequestDto {
 
-    private String memberId;
-    private String memberPw;
+    private GameData gameData;
+    private TeamData teamData;
 }
