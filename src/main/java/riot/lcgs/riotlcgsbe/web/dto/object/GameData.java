6c3f19a19a1961c8a32c712ce9cc9dc949fd860f
rlawo <rlawo32@naver.com>
2024-12-09 22:58:20 +0900

    [RiotLCGS][BE] <Dto> DataObject_작업_20241209 (6)

---
+++ src/main/java/riot/lcgs/riotlcgsbe/web/dto/object/GameData.java
@@ -0,0 +1,23 @@
+package riot.lcgs.riotlcgsbe.web.dto.object;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class GameData {
+    private String endOfGameResult;
+    private Long gameCreation;
+    private String gameCreationDate;
+    private Long gameDuration;
+    private Long gameId;
+    private String gameMode;
+    private String gameType;
+    private String gameVersion;
+    private Long mapId;
+    private List<ParticipantIdentities> participantIdentities;
+    private List<Participants> participants;
+    private Long queueId;
+    private Long seasonId;
+    private List<Teams> teams;
+}
