37ec043e069c38ac752b917f80a1e24dc98f6eb3
rlawo <rlawo32@naver.com>
2024-12-10 23:23:49 +0900

    [RiotLCGS][BE] <Controller> Main_작업_20241209 (3)

--- src/main/java/riot/lcgs/riotlcgsbe/web/controller/MainController.java
+++ src/main/java/riot/lcgs/riotlcgsbe/web/controller/MainController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import riot.lcgs.riotlcgsbe.service.MainService;
+import riot.lcgs.riotlcgsbe.web.dto.CommonResponseDto;
 import riot.lcgs.riotlcgsbe.web.dto.CustomGameRequestDto;
 
 @RequiredArgsConstructor
@@ -12,11 +14,13 @@ import riot.lcgs.riotlcgsbe.web.dto.CustomGameRequestDto;
 @RequestMapping("/riot")
 public class MainController {
 
+    private final MainService mainService;
+
     @PostMapping("/insertData")
-    public String insertData(@RequestBody CustomGameRequestDto requestDto) {
+    public CommonResponseDto<?> insertData(@RequestBody CustomGameRequestDto requestDto) {
         System.out.println(requestDto.getGameData().getGameCreation());
         System.out.println(requestDto.getGameData().getParticipantIdentities().get(0).getPlayer().getSummonerName());
-        return "";
+        return mainService.GameDataOrganizeService(requestDto);
     }
 
 }
