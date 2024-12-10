14a57241f3bf1396b14b08f64a52c733d46f2114
rlawo <rlawo32@naver.com>
2024-12-10 23:32:13 +0900

    [RiotLCGS][BE] <Supabase> 초기설정_작업_20241209 (1)

--- src/main/java/riot/lcgs/riotlcgsbe/RiotLcgsBeApplication.java
+++ src/main/java/riot/lcgs/riotlcgsbe/RiotLcgsBeApplication.java
@@ -4,7 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+// @SpringBootApplication(exclude={DataSourceAutoConfiguration.class}) // DB 연결 확인 무시
+@SpringBootApplication
 public class RiotLcgsBeApplication {
 
 	public static void main(String[] args) {
