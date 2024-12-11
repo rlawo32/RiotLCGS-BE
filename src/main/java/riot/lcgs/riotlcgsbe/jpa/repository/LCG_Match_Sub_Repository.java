62f86df3b9e11d6ba59cdd71afde47f3aa6786b2
rlawo <rlawo32@naver.com>
2024-12-12 00:01:17 +0900

    [RiotLCGS][BE] <JPA> Repository_추가_작업_20241212 (1)

---
+++ src/main/java/riot/lcgs/riotlcgsbe/jpa/repository/LCG_Match_Sub_Repository.java
@@ -0,0 +1,8 @@
+package riot.lcgs.riotlcgsbe.jpa.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import riot.lcgs.riotlcgsbe.jpa.domain.LCG_Match_Sub;
+
+public interface LCG_Match_Sub_Repository extends JpaRepository<LCG_Match_Sub, Long> {
+
+}
---
+++ src/main/java/riot/lcgs/riotlcgsbe/jpa/repository/LCG_Match_Team_Repository.java
@@ -0,0 +1,8 @@
+package riot.lcgs.riotlcgsbe.jpa.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import riot.lcgs.riotlcgsbe.jpa.domain.LCG_Match_Team;
+
+public interface LCG_Match_Team_Repository extends JpaRepository<LCG_Match_Team, Long> {
+
+}
