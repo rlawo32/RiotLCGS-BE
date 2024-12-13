fbea276574121a661a17f39a9db9b14905eeb346
rlawo <rlawo32@naver.com>
2024-12-13 21:23:31 +0900

    [RiotLCGS][BE] <DataDragon> Version_저장_작업_20241213 (2)

--- src/main/java/riot/lcgs/riotlcgsbe/jpa/domain/LCG_Match_Info.java
+++ src/main/java/riot/lcgs/riotlcgsbe/jpa/domain/LCG_Match_Info.java
@@ -33,14 +33,49 @@ public class LCG_Match_Info {
     @Column(name = "lcg_game_map")
     private int lgcGameMap;
 
+    @Column(name = "lcg_ver_main")
+    private String lgcVerMain;
+
+    @Column(name = "lcg_ver_cdn")
+    private String lgcVerCdn;
+
+    @Column(name = "lcg_ver_lang")
+    private String lgcVerLang;
+
+    @Column(name = "lcg_ver_item")
+    private String lgcVerItem;
+
+    @Column(name = "lcg_ver_rune")
+    private String lgcVerRune;
+
+    @Column(name = "lcg_ver_mastery")
+    private String lgcVerMastery;
+
+    @Column(name = "lcg_ver_summoner")
+    private String lgcVerSummoner;
+
+    @Column(name = "lcg_ver_champion")
+    private String lgcVerChampion;
+
     @Builder
     public LCG_Match_Info(Long lgcGameId, String lgcGameDate, String lgcGameMode,
-                          String lgcGameType, int lgcGameDuration, int lgcGameMap) {
+                          String lgcGameType, int lgcGameDuration, int lgcGameMap,
+                          String lgcVerMain, String lgcVerCdn, String lgcVerLang,
+                          String lgcVerItem, String lgcVerRune, String lgcVerMastery,
+                          String lgcVerSummoner, String lgcVerChampion) {
         this.lgcGameId = lgcGameId;
         this.lgcGameDate = lgcGameDate;
         this.lgcGameMode = lgcGameMode;
         this.lgcGameType = lgcGameType;
         this.lgcGameDuration = lgcGameDuration;
         this.lgcGameMap = lgcGameMap;
+        this.lgcVerMain = lgcVerMain;
+        this.lgcVerCdn = lgcVerCdn;
+        this.lgcVerLang = lgcVerLang;
+        this.lgcVerItem = lgcVerItem;
+        this.lgcVerRune = lgcVerRune;
+        this.lgcVerMastery = lgcVerMastery;
+        this.lgcVerSummoner = lgcVerSummoner;
+        this.lgcVerChampion = lgcVerChampion;
     }
 }
