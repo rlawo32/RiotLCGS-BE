d9eab9117be404c3f498e0bd2aaf859bcc922c47
rlawo <rlawo32@naver.com>
2024-12-13 21:23:59 +0900

    [RiotLCGS][BE] <Service> 객체_분리_작업_20241213 (1)

--- src/main/java/riot/lcgs/riotlcgsbe/service/MainService.java
+++ src/main/java/riot/lcgs/riotlcgsbe/service/MainService.java
@@ -14,22 +14,13 @@ import riot.lcgs.riotlcgsbe.web.dto.CommonResponseDto;
 import riot.lcgs.riotlcgsbe.web.dto.CustomGameRequestDto;
 import riot.lcgs.riotlcgsbe.web.dto.object.*;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.List;
 
 @RequiredArgsConstructor
 @Service
 public class MainService {
 
-    private final LCG_Match_Info_Repository lcgMatchInfoRepository;
-    private final LCG_Match_Main_Repository lcgMatchMainRepository;
-    private final LCG_Match_Sub_Repository lcgMatchSubRepository;
-    private final LCG_Match_Team_Repository lcgMatchTeamRepository;
+    private final HttpService httpService;
 
     public CommonResponseDto<?> LolCustomGameDataSave(CustomGameRequestDto requestDto) {
 
@@ -38,153 +29,14 @@ public class MainService {
         Long gameId = gameData.getGameId();
 
         // 동일한 gameId로 insert 불가
+        // info 테이블에 각 게임 데이터(champion, item ...) 버전 insert (필드 새로 생성해야함)
+        // spellId는 DB에 넣기 (자동화 만들기, 게임 버전이 변경될 때 마다 업데이트)
+        // json 통신 데이터 자바 객체에 매핑 하기
+        // spellId, perkId, championId 는 이미지 주소가 id 값이 아닌 name 값으로 되어 있기에 json 에서 name 추출해야함 (item 은 id 값임)
 
-        /*
-        lcgMatchInfoRepository.save(LCG_Match_Info.builder()
-                .lgcGameId(gameId)
-                .lgcGameDate(gameData.getGameCreationDate())
-                .lgcGameMode(gameData.getGameMode())
-                .lgcGameType(gameData.getGameType())
-                .lgcGameDuration(gameData.getGameDuration())
-                .lgcGameMap(gameData.getMapId()).build());
-         */
-        List<ParticipantIdentities> list1 = gameData.getParticipantIdentities();
-        List<Participants> list2 = gameData.getParticipants();
-        List<Teams> list3 = gameData.getTeams();
+        CommonResponseDto<?> commonResponseDto = httpService.DataDragonAPIVersion();
 
-        int matchLen = list1.size();
-        int teamsLen = list3.size();
-
-        DataDragonAPIVersion();
-/*
-        for(int i=0; i<matchLen; i++) {
-            int gbnId = list1.get(i).getParticipantId();
-
-            ParticipantIdentities participantIdentities = list1.get(i);
-            Participants participants = list2.get(i);
-            Player playerData = participantIdentities.getPlayer();
-            Stats statsData = participants.getStats();
-
-            lcgMatchMainRepository.save(LCG_Match_Main.builder()
-                    .lgcGameId(gameId)
-                    .lgcParticipantId(participantIdentities.getParticipantId())
-                    .lgcTeamId(participants.getTeamId())
-                    .lgcSummonerId(playerData.getSummonerId())
-                    .lgcSummonerName(playerData.getSummonerName())
-                    .lgcSummonerTag(playerData.getTagLine())
-                    .lgcChampionId(participants.getChampionId())
-                    .lgcChampionName("NONE")
-                    .lgcChampionLevel(statsData.getChampLevel())
-                    .lgcSpellId1(participants.getSpell1Id()).lgcSpellId2(participants.getSpell2Id())
-                    .lgcPerkId1(statsData.getPerk0()).lgcPerkId2(statsData.getPerkSubStyle())
-                    .lgcItemId1(statsData.getItem0()).lgcItemId2(statsData.getItem1())
-                    .lgcItemId3(statsData.getItem2()).lgcItemId4(statsData.getItem3())
-                    .lgcItemId5(statsData.getItem4()).lgcItemId6(statsData.getItem5()).lgcItemId7(statsData.getItem6())
-                    .lgcKillCount(statsData.getKills())
-                    .lgcAssistCount(statsData.getAssists())
-                    .lgcDeathCount(statsData.getDeaths())
-                    .lgcDamageTotal(statsData.getTotalDamageDealtToChampions())
-                    .lgcDamageTaken(statsData.getTotalDamageTaken())
-                    .lgcMinionCount(statsData.getTotalMinionsKilled())
-                    .lgcJungleCount(statsData.getNeutralMinionsKilled())
-                    .lgcVisionScore(statsData.getVisionScore()).build());
-
-            lcgMatchSubRepository.save(LCG_Match_Sub.builder()
-                    .lgcGameId(gameId)
-                    .lgcParticipantId(participantIdentities.getParticipantId())
-                    .lgcFirstKill(statsData.isFirstBloodKill() ? "Y" : "N")
-                    .lgcFirstTower(statsData.isFirstTowerKill() ? "Y" : "N")
-                    .lgcDoubleKill(statsData.getDoubleKills())
-                    .lgcTripleKill(statsData.getTripleKills())
-                    .lgcQuadraKill(statsData.getQuadraKills())
-                    .lgcPentaKill(statsData.getPentaKills())
-                    .lgcNormalWard(statsData.getWardsPlaced())
-                    .lgcVisionWard(statsData.getVisionWardsBoughtInGame())
-                    .lgcGoldTotal(statsData.getGoldEarned())
-                    .lgcHealTotal(statsData.getTotalHeal())
-                    .lgcCrowdTime(statsData.getTimeCCingOthers())
-                    .lgcDestroyTower(statsData.getTurretKills())
-                    .lgcDamageTower(statsData.getDamageDealtToTurrets()).build());
-        }
-
-
-        for(int i=0; i<teamsLen; i++) {
-            Teams teams = list3.get(i);
-            List<Bans> bans = teams.getBans();
-
-            lcgMatchTeamRepository.save(LCG_Match_Team.builder()
-                    .lgcGameId(gameId)
-                    .lgcTeamId(teams.getTeamId())
-                    .lgcFirstDragon(teams.isFirstDargon() ? "Y" : "N")
-                    .lgcFirstBaron(teams.isFirstBaron() ? "Y" : "N")
-                    .lgcFirstKill(teams.isFirstBlood() ? "Y" : "N")
-                    .lgcFirstTower(teams.isFirstTower() ? "Y" : "N")
-                    .lgcFirstInhibitor(teams.isFirstInhibitor() ? "Y" : "N")
-                    .lgcDragonTotal(teams.getDragonKills())
-                    .lgcBaronTotal(teams.getBaronKills())
-                    .lgcTowerTotal(teams.getTowerKills())
-                    .lgcHordeTotal(teams.getHordeKills())
-                    .lgcHeraldTotal(teams.getRiftHeraldKills())
-                    .lgcBansChamp1(teams.getTeamId() == 100 ? bans.get(0).getChampionId() : 0)
-                    .lgcBansName1("")
-                    .lgcBansChamp2(teams.getTeamId() == 100 ? bans.get(1).getChampionId() : 0)
-                    .lgcBansName2("")
-                    .lgcBansChamp3(teams.getTeamId() == 100 ? bans.get(2).getChampionId() : 0)
-                    .lgcBansName3("")
-                    .lgcBansChamp4(teams.getTeamId() == 100 ? bans.get(3).getChampionId() : 0)
-                    .lgcBansName4("")
-                    .lgcBansChamp5(teams.getTeamId() == 100 ? bans.get(4).getChampionId() : 0)
-                    .lgcBansName5("")
-                    .lgcBansChamp6(teams.getTeamId() == 200 ? bans.get(0).getChampionId() : 0)
-                    .lgcBansName6("")
-                    .lgcBansChamp7(teams.getTeamId() == 200 ? bans.get(1).getChampionId() : 0)
-                    .lgcBansName7("")
-                    .lgcBansChamp8(teams.getTeamId() == 200 ? bans.get(2).getChampionId() : 0)
-                    .lgcBansName8("")
-                    .lgcBansChamp9(teams.getTeamId() == 200 ? bans.get(3).getChampionId() : 0)
-                    .lgcBansName9("")
-                    .lgcBansChamp10(teams.getTeamId() == 200 ? bans.get(4).getChampionId() : 0)
-                    .lgcBansName10("")
-                    .build());
-
-        }
-
- */
-
-        return CommonResponseDto.setSuccess("Success", "");
-    }
-
-    public CommonResponseDto<?> DataDragonAPIVersion() {
-
-        try {
-            URL url = new URL("https://ddragon.leagueoflegends.com/realms/na.json");
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-
-            conn.setRequestMethod("GET");
-            conn.setRequestProperty("Content-type", "application/json");
-            conn.setDoOutput(true);
-
-            BufferedReader br = null;
-            if(conn.getResponseCode() == 200) {
-                br = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
-            } else {
-                br = new BufferedReader(new InputStreamReader(conn.getErrorStream(), "UTF-8"));
-            }
-
-            StringBuffer sb = new StringBuffer();
-            String line;
-            while ((line = br.readLine()) != null) {
-                sb.append(line);
-            }
-
-            br.close();
-            conn.disconnect();
-
-            System.out.println(sb.toString());
-        } catch (IOException ex1) {
-            ex1.printStackTrace();
-            return CommonResponseDto.setSuccess("Failed", ex1.getMessage());
-        }
+        System.out.println(commonResponseDto.getData());
 
 
         return CommonResponseDto.setSuccess("Success", "");
