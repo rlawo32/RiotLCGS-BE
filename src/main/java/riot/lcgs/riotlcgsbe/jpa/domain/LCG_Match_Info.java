ef7465278273bd1ccc2c43756f8a650d79559f24
rlawo <rlawo32@naver.com>
2024-12-15 03:24:45 +0900

    [RiotLCGS][BE] <JPA> LCG_Match_Info_Column_변경_20241215 (1)

--- src/main/java/riot/lcgs/riotlcgsbe/jpa/domain/LCG_Match_Info.java
+++ src/main/java/riot/lcgs/riotlcgsbe/jpa/domain/LCG_Match_Info.java
@@ -16,66 +16,66 @@ public class LCG_Match_Info {
 
     @Id
     @Column(name = "lcg_game_id")
-    private Long lgcGameId;
+    private Long lcgGameId;
 
     @Column(name = "lcg_game_date")
-    private String lgcGameDate;
+    private String lcgGameDate;
 
     @Column(name = "lcg_game_mode")
-    private String lgcGameMode;
+    private String lcgGameMode;
 
     @Column(name = "lcg_game_type")
-    private String lgcGameType;
+    private String lcgGameType;
 
     @Column(name = "lcg_game_duration")
-    private int lgcGameDuration;
+    private int lcgGameDuration;
 
     @Column(name = "lcg_game_map")
-    private int lgcGameMap;
+    private int lcgGameMap;
 
     @Column(name = "lcg_ver_main")
-    private String lgcVerMain;
+    private String lcgVerMain;
 
     @Column(name = "lcg_ver_cdn")
-    private String lgcVerCdn;
+    private String lcgVerCdn;
 
     @Column(name = "lcg_ver_lang")
-    private String lgcVerLang;
+    private String lcgVerLang;
 
     @Column(name = "lcg_ver_item")
-    private String lgcVerItem;
+    private String lcgVerItem;
 
     @Column(name = "lcg_ver_rune")
-    private String lgcVerRune;
+    private String lcgVerRune;
 
     @Column(name = "lcg_ver_mastery")
-    private String lgcVerMastery;
+    private String lcgVerMastery;
 
     @Column(name = "lcg_ver_summoner")
-    private String lgcVerSummoner;
+    private String lcgVerSummoner;
 
     @Column(name = "lcg_ver_champion")
-    private String lgcVerChampion;
+    private String lcgVerChampion;
 
     @Builder
-    public LCG_Match_Info(Long lgcGameId, String lgcGameDate, String lgcGameMode,
-                          String lgcGameType, int lgcGameDuration, int lgcGameMap,
-                          String lgcVerMain, String lgcVerCdn, String lgcVerLang,
-                          String lgcVerItem, String lgcVerRune, String lgcVerMastery,
-                          String lgcVerSummoner, String lgcVerChampion) {
-        this.lgcGameId = lgcGameId;
-        this.lgcGameDate = lgcGameDate;
-        this.lgcGameMode = lgcGameMode;
-        this.lgcGameType = lgcGameType;
-        this.lgcGameDuration = lgcGameDuration;
-        this.lgcGameMap = lgcGameMap;
-        this.lgcVerMain = lgcVerMain;
-        this.lgcVerCdn = lgcVerCdn;
-        this.lgcVerLang = lgcVerLang;
-        this.lgcVerItem = lgcVerItem;
-        this.lgcVerRune = lgcVerRune;
-        this.lgcVerMastery = lgcVerMastery;
-        this.lgcVerSummoner = lgcVerSummoner;
-        this.lgcVerChampion = lgcVerChampion;
+    public LCG_Match_Info(Long lcgGameId, String lcgGameDate, String lcgGameMode,
+                          String lcgGameType, int lcgGameDuration, int lcgGameMap,
+                          String lcgVerMain, String lcgVerCdn, String lcgVerLang,
+                          String lcgVerItem, String lcgVerRune, String lcgVerMastery,
+                          String lcgVerSummoner, String lcgVerChampion) {
+        this.lcgGameId = lcgGameId;
+        this.lcgGameDate = lcgGameDate;
+        this.lcgGameMode = lcgGameMode;
+        this.lcgGameType = lcgGameType;
+        this.lcgGameDuration = lcgGameDuration;
+        this.lcgGameMap = lcgGameMap;
+        this.lcgVerMain = lcgVerMain;
+        this.lcgVerCdn = lcgVerCdn;
+        this.lcgVerLang = lcgVerLang;
+        this.lcgVerItem = lcgVerItem;
+        this.lcgVerRune = lcgVerRune;
+        this.lcgVerMastery = lcgVerMastery;
+        this.lcgVerSummoner = lcgVerSummoner;
+        this.lcgVerChampion = lcgVerChampion;
     }
 }
