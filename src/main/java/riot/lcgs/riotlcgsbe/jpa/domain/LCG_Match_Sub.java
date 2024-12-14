a97e3fcc76cae1a2a3deee19a2964ac79fc0faa4
rlawo <rlawo32@naver.com>
2024-12-15 03:24:33 +0900

    [RiotLCGS][BE] <JPA> LCG_Match_Sub_Column_변경_20241215 (1)

--- src/main/java/riot/lcgs/riotlcgsbe/jpa/domain/LCG_Match_Sub.java
+++ src/main/java/riot/lcgs/riotlcgsbe/jpa/domain/LCG_Match_Sub.java
@@ -17,69 +17,69 @@ public class LCG_Match_Sub {
     private Long rowNum;
 
     @Column(name = "lcg_game_id")
-    private Long lgcGameId;
+    private Long lcgGameId;
 
     @Column(name = "lcg_participant_id")
-    private int lgcParticipantId;
+    private int lcgParticipantId;
 
     @Column(name = "lcg_first_kill", length = 1)
-    private String lgcFirstKill;
+    private String lcgFirstKill;
 
     @Column(name = "lcg_first_tower", length = 1)
-    private String lgcFirstTower;
+    private String lcgFirstTower;
 
     @Column(name = "lcg_double_kill")
-    private int lgcDoubleKill;
+    private int lcgDoubleKill;
 
     @Column(name = "lcg_triple_kill")
-    private int lgcTripleKill;
+    private int lcgTripleKill;
 
     @Column(name = "lcg_quadra_kill")
-    private int lgcQuadraKill;
+    private int lcgQuadraKill;
 
     @Column(name = "lcg_penta_kill")
-    private int lgcPentaKill;
+    private int lcgPentaKill;
 
     @Column(name = "lcg_normal_ward")
-    private int lgcNormalWard;
+    private int lcgNormalWard;
 
     @Column(name = "lcg_vision_ward")
-    private int lgcVisionWard;
+    private int lcgVisionWard;
 
     @Column(name = "lcg_gold_total")
-    private int lgcGoldTotal;
+    private int lcgGoldTotal;
 
     @Column(name = "lcg_heal_total")
-    private int lgcHealTotal;
+    private int lcgHealTotal;
 
     @Column(name = "lcg_crowd_time")
-    private int lgcCrowdTime;
+    private int lcgCrowdTime;
 
     @Column(name = "lcg_destroy_tower")
-    private int lgcDestroyTower;
+    private int lcgDestroyTower;
 
     @Column(name = "lcg_damage_tower")
-    private int lgcDamageTower;
+    private int lcgDamageTower;
 
     @Builder
-    public LCG_Match_Sub(Long lgcGameId, int lgcParticipantId, String lgcFirstKill, String lgcFirstTower,
-                         int lgcDoubleKill, int lgcTripleKill, int lgcQuadraKill, int lgcPentaKill,
-                         int lgcNormalWard, int lgcVisionWard, int lgcGoldTotal, int lgcHealTotal,
-                         int lgcCrowdTime, int lgcDestroyTower, int lgcDamageTower) {
-        this.lgcGameId = lgcGameId;
-        this.lgcParticipantId = lgcParticipantId;
-        this.lgcFirstKill = lgcFirstKill;
-        this.lgcFirstTower = lgcFirstTower;
-        this.lgcDoubleKill = lgcDoubleKill;
-        this.lgcTripleKill = lgcTripleKill;
-        this.lgcQuadraKill = lgcQuadraKill;
-        this.lgcPentaKill = lgcPentaKill;
-        this.lgcNormalWard = lgcNormalWard;
-        this.lgcVisionWard = lgcVisionWard;
-        this.lgcGoldTotal = lgcGoldTotal;
-        this.lgcHealTotal = lgcHealTotal;
-        this.lgcCrowdTime = lgcCrowdTime;
-        this.lgcDestroyTower = lgcDestroyTower;
-        this.lgcDamageTower = lgcDamageTower;
+    public LCG_Match_Sub(Long lcgGameId, int lcgParticipantId, String lcgFirstKill, String lcgFirstTower,
+                         int lcgDoubleKill, int lcgTripleKill, int lcgQuadraKill, int lcgPentaKill,
+                         int lcgNormalWard, int lcgVisionWard, int lcgGoldTotal, int lcgHealTotal,
+                         int lcgCrowdTime, int lcgDestroyTower, int lcgDamageTower) {
+        this.lcgGameId = lcgGameId;
+        this.lcgParticipantId = lcgParticipantId;
+        this.lcgFirstKill = lcgFirstKill;
+        this.lcgFirstTower = lcgFirstTower;
+        this.lcgDoubleKill = lcgDoubleKill;
+        this.lcgTripleKill = lcgTripleKill;
+        this.lcgQuadraKill = lcgQuadraKill;
+        this.lcgPentaKill = lcgPentaKill;
+        this.lcgNormalWard = lcgNormalWard;
+        this.lcgVisionWard = lcgVisionWard;
+        this.lcgGoldTotal = lcgGoldTotal;
+        this.lcgHealTotal = lcgHealTotal;
+        this.lcgCrowdTime = lcgCrowdTime;
+        this.lcgDestroyTower = lcgDestroyTower;
+        this.lcgDamageTower = lcgDamageTower;
     }
 }
