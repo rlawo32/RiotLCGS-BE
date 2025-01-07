7a388da986df747218ee1ff2c1469edfd5058506
rlawo <rlawo32@naver.com>
2025-01-07 23:16:18 +0900

    [RiotLCGS][BE] <JPA> Entity_Notnull_추가_20250107 (1)

--- src/main/java/riot/lcgs/riotlcgsbe/jpa/domain/LCG_Match_Info.java
+++ src/main/java/riot/lcgs/riotlcgsbe/jpa/domain/LCG_Match_Info.java
@@ -1,9 +1,11 @@
 package riot.lcgs.riotlcgsbe.jpa.domain;
 
+import jakarta.annotation.Nonnull;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,51 +17,67 @@ import lombok.NoArgsConstructor;
 public class LCG_Match_Info {
 
     @Id
+    @NotNull
     @Column(name = "lcg_game_id")
     private Long lcgGameId;
 
+    @NotNull
     @Column(name = "lcg_game_date")
     private String lcgGameDate;
 
+    @NotNull
     @Column(name = "lcg_game_mode")
     private String lcgGameMode;
 
+    @NotNull
     @Column(name = "lcg_game_type")
     private String lcgGameType;
 
+    @NotNull
     @Column(name = "lcg_game_duration")
     private int lcgGameDuration;
 
+    @NotNull
     @Column(name = "lcg_game_map")
     private int lcgGameMap;
 
+    @NotNull
     @Column(name = "lcg_ver_main")
     private String lcgVerMain;
 
+    @NotNull
     @Column(name = "lcg_ver_cdn")
     private String lcgVerCdn;
 
+    @NotNull
     @Column(name = "lcg_ver_lang")
     private String lcgVerLang;
 
+    @NotNull
     @Column(name = "lcg_ver_item")
     private String lcgVerItem;
 
+    @NotNull
     @Column(name = "lcg_ver_rune")
     private String lcgVerRune;
 
+    @NotNull
     @Column(name = "lcg_ver_mastery")
     private String lcgVerMastery;
 
+    @NotNull
     @Column(name = "lcg_ver_summoner")
     private String lcgVerSummoner;
 
+    @NotNull
     @Column(name = "lcg_ver_champion")
     private String lcgVerChampion;
 
+    @NotNull
     @Column(name = "lcg_max_damage_total")
     private int lcgMaxDamageTotal;
 
+    @NotNull
     @Column(name = "lcg_max_damage_taken")
     private int lcgMaxDamageTaken;
 
