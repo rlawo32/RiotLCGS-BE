c3e6453022a879ed098a50668ed9e92589ecaeca
rlawo <rlawo32@naver.com>
2025-01-07 23:31:11 +0900

    [RiotLCGS][BE] <JPA> LCG_Player_Statistics_작업_20250107 (2)

---
+++ src/main/java/riot/lcgs/riotlcgsbe/jpa/repository/LCG_Player_Statistics_Repository.java
@@ -0,0 +1,8 @@
+package riot.lcgs.riotlcgsbe.jpa.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import riot.lcgs.riotlcgsbe.jpa.domain.LCG_Player_Statistics;
+
+public interface LCG_Player_Statistics_Repository extends JpaRepository<LCG_Player_Statistics, String> {
+    boolean existsLCG_Player_StatisticsByLcgPuuid(String puuid);
+}
