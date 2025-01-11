738aff65f8e3b31a25540369e6269eceb03b1650
rlawo <rlawo32@naver.com>
2025-01-12 00:48:14 +0900

    [RiotLCGS][BE] <JPA> LCG_Match_Statistics_Column(Puuid 수정)_20250111

--- src/main/java/riot/lcgs/riotlcgsbe/jpa/repository/LCG_Player_Statistics_Repository.java
+++ src/main/java/riot/lcgs/riotlcgsbe/jpa/repository/LCG_Player_Statistics_Repository.java
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import riot.lcgs.riotlcgsbe.jpa.domain.LCG_Player_Statistics;
 
 public interface LCG_Player_Statistics_Repository extends JpaRepository<LCG_Player_Statistics, String> {
-    boolean existsLCG_Player_StatisticsByLcgPuuid(String puuid);
+    boolean existsLCG_Player_StatisticsByLcgSummonerPuuid(String puuid);
 }
