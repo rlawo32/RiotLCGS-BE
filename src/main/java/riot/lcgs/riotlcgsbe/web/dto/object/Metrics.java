88bd40998cd495b4ac7a9c849bb3c9e259c2bdec
rlawo <rlawo32@naver.com>
2025-01-24 02:17:39 +0900

    [RiotLCGS][BE] <Dto> Metrics_객체_작업_20250124 (2)

--- src/main/java/riot/lcgs/riotlcgsbe/web/dto/object/Metrics.java
+++ src/main/java/riot/lcgs/riotlcgsbe/web/dto/object/Metrics.java
@@ -4,5 +4,23 @@ import lombok.Data;
 
 @Data
 public class Metrics {
-    
+    private String puuid;
+    private int kill;
+    private int death;
+    private int assist;
+    private int gold;
+    private int cs;
+    private int damage;
+    private int visionScore;
+    private int objectScore;
+    private int multiKillScore;
+    private int demolisherScore;
+    private int totalKill;
+    private int totalScore;
+    private boolean firstKill;
+    private boolean firstTower;
+    private boolean firstDragon;
+    private boolean firstBaron;
+    private boolean firstInhibitor;
+    private boolean win;
 }
