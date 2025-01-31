7a969e956a13d73af24096b2fc1bb8e9aced4abf
rlawo <rlawo32@naver.com>
2025-02-01 04:03:06 +0900

    [RiotLCGS][BE] <Dto> Metrics_객체_작업_20250201 (7)

--- src/main/java/riot/lcgs/riotlcgsbe/web/dto/object/Metrics.java
+++ src/main/java/riot/lcgs/riotlcgsbe/web/dto/object/Metrics.java
@@ -7,6 +7,7 @@ public class Metrics {
     private String puuid;
     private String nickname;
     private int totalScore;
+    private int team;
     private int kill;
     private int death;
     private int assist;
@@ -31,13 +32,14 @@ public class Metrics {
     private boolean firstBaron;
 
 
-    public Metrics(String puuid, String nickname, int totalScore, int kill, int death, int assist, double kda,
+    public Metrics(String puuid, String nickname, int totalScore, int team, int kill, int death, int assist, double kda,
                    int engagementRate, int gold, int damage, int cs, double dpm, double gpm, double dpg, int visionScore,
                    int crowdScore, long multiKillScore, long demolisherScore, long objectScore, boolean win,
                    boolean firstKill, boolean firstTower, boolean firstInhibitor, boolean firstDragon, boolean firstBaron) {
         this.puuid = puuid;
         this.nickname = nickname;
         this.totalScore = totalScore;
+        this.team = team;
         this.kill = kill;
         this.death = death;
         this.assist = assist;
