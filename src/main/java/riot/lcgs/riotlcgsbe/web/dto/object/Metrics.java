9a197fe9f749a4c22fe6da95bee7daa92a4808d0
rlawo <rlawo32@naver.com>
2025-02-01 02:04:58 +0900

    [RiotLCGS][BE] <Dto> Metrics_객체_작업_20250201 (6)

--- src/main/java/riot/lcgs/riotlcgsbe/web/dto/object/Metrics.java
+++ src/main/java/riot/lcgs/riotlcgsbe/web/dto/object/Metrics.java
@@ -5,19 +5,24 @@ import lombok.Data;
 @Data
 public class Metrics {
     private String puuid;
+    private String nickname;
+    private int totalScore;
     private int kill;
     private int death;
     private int assist;
+    private double kda;
+    private int engagementRate;
     private int gold;
-    private int cs;
     private int damage;
+    private int cs;
+    private double dpm;
+    private double gpm;
+    private double dpg;
     private int visionScore;
     private int crowdScore;
     private long multiKillScore;
     private long demolisherScore;
     private long objectScore;
-    private int totalKill;
-    private int totalScore;
     private boolean win;
     private boolean firstKill;
     private boolean firstTower;
@@ -26,24 +31,29 @@ public class Metrics {
     private boolean firstBaron;
 
 
-    public Metrics(String puuid, int kill, int death, int assist, int gold, int cs, int damage,
-                   int visionScore, int crowdScore, long multiKillScore, long demolisherScore,
-                   long objectScore, int totalKill, int totalScore, boolean win, boolean firstKill,
-                   boolean firstTower, boolean firstInhibitor, boolean firstDragon, boolean firstBaron) {
+    public Metrics(String puuid, String nickname, int totalScore, int kill, int death, int assist, double kda,
+                   int engagementRate, int gold, int damage, int cs, double dpm, double gpm, double dpg, int visionScore,
+                   int crowdScore, long multiKillScore, long demolisherScore, long objectScore, boolean win,
+                   boolean firstKill, boolean firstTower, boolean firstInhibitor, boolean firstDragon, boolean firstBaron) {
         this.puuid = puuid;
+        this.nickname = nickname;
+        this.totalScore = totalScore;
         this.kill = kill;
         this.death = death;
         this.assist = assist;
+        this.kda = kda;
+        this.engagementRate = engagementRate;
         this.gold = gold;
-        this.cs = cs;
         this.damage = damage;
+        this.cs = cs;
+        this.dpm = dpm;
+        this.gpm = gpm;
+        this.dpg = dpg;
         this.visionScore = visionScore;
         this.crowdScore = crowdScore;
         this.multiKillScore = multiKillScore;
         this.demolisherScore = demolisherScore;
         this.objectScore = objectScore;
-        this.totalKill = totalKill;
-        this.totalScore = totalScore;
         this.win = win;
         this.firstKill = firstKill;
         this.firstTower = firstTower;
