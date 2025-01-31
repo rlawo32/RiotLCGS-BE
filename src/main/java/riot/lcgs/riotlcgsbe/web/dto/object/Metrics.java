76402a1bf32500ceeefeb9a5a270b5113780f13f
rlawo <rlawo32@naver.com>
2025-01-31 23:32:07 +0900

    [RiotLCGS][BE] <Dto> Metrics_객체_작업_20250131 (4)

--- src/main/java/riot/lcgs/riotlcgsbe/web/dto/object/Metrics.java
+++ src/main/java/riot/lcgs/riotlcgsbe/web/dto/object/Metrics.java
@@ -12,23 +12,23 @@ public class Metrics {
     private int cs;
     private int damage;
     private int visionScore;
-    private int objectScore;
-    private int multiKillScore;
-    private int demolisherScore;
+    private long multiKillScore;
+    private long demolisherScore;
+    private long objectScore;
     private int totalKill;
     private int totalScore;
+    private boolean win;
     private boolean firstKill;
     private boolean firstTower;
+    private boolean firstInhibitor;
     private boolean firstDragon;
     private boolean firstBaron;
-    private boolean firstInhibitor;
-    private boolean win;
 
 
     public Metrics(String puuid, int kill, int death, int assist, int gold, int cs, int damage,
-                   int visionScore, int objectScore, int multiKillScore, int demolisherScore,
-                   int totalKill, int totalScore, boolean firstKill, boolean firstTower,
-                   boolean firstDragon, boolean firstBaron, boolean firstInhibitor, boolean win) {
+                   int visionScore, long multiKillScore, long demolisherScore, long objectScore,
+                   int totalKill, int totalScore, boolean win, boolean firstKill, boolean firstTower,
+                   boolean firstInhibitor, boolean firstDragon, boolean firstBaron) {
         this.puuid = puuid;
         this.kill = kill;
         this.death = death;
@@ -37,16 +37,16 @@ public class Metrics {
         this.cs = cs;
         this.damage = damage;
         this.visionScore = visionScore;
-        this.objectScore = objectScore;
         this.multiKillScore = multiKillScore;
         this.demolisherScore = demolisherScore;
+        this.objectScore = objectScore;
         this.totalKill = totalKill;
         this.totalScore = totalScore;
+        this.win = win;
         this.firstKill = firstKill;
         this.firstTower = firstTower;
+        this.firstInhibitor = firstInhibitor;
         this.firstDragon = firstDragon;
         this.firstBaron = firstBaron;
-        this.firstInhibitor = firstInhibitor;
-        this.win = win;
     }
 }
