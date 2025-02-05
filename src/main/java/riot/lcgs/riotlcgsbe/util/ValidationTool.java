87b150570aa5f9e705f7619205cf702b0a1d6459
sungjae <92013056+rlawo32@users.noreply.github.com>
2025-02-05 13:10:16 +0900

    [RiotLCGS][BE] <Util> ValidationTool_작업_20250205 (2)

--- src/main/java/riot/lcgs/riotlcgsbe/util/ValidationTool.java
+++ src/main/java/riot/lcgs/riotlcgsbe/util/ValidationTool.java
@@ -2,4 +2,35 @@ package riot.lcgs.riotlcgsbe.util;
 
 public class ValidationTool {
   
+    public static CommonResponseDto<String> validationChkString(String param) {
+		    if(param == null || param.length() == 0) {
+			    throw new NullPointerException();
+		    } else {
+			    return CommonResponseDto.setSuccess("Success", param);
+		    }
+	  }
+
+    public static CommonResponseDto<Integer> validationChkInteger(Integer param) {
+		    if(param == null) {
+			    throw new NullPointerException();
+		    } else {
+			    return CommonResponseDto.setSuccess("Success", param);
+		    }
+	  }
+  
+    public static CommonResponseDto<Long> validationChkLong(Long param) {
+		    if(param == null) {
+			    throw new NullPointerException();
+		    } else {
+			    return CommonResponseDto.setSuccess("Success", param);
+		    }
+	  }
+  
+    public static CommonResponseDto<Boolean> validationChkBoolean(Boolean param) {
+		    if(param == null) {
+			    throw new NullPointerException();
+		    } else {
+			    return CommonResponseDto.setSuccess("Success", param);
+		    }
+	  }
 }
