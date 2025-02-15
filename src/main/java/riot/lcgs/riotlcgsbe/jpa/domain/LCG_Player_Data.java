79176d722719450718cba1d16d2d4d02bd1a7ef9
rlawo <rlawo32@naver.com>
2025-02-16 01:59:22 +0900

    [RiotLCGS][BE] <JPA> LcgPlayerDat_저장/업데이트_작업_20250216

--- src/main/java/riot/lcgs/riotlcgsbe/jpa/domain/LCG_Player_Data.java
+++ src/main/java/riot/lcgs/riotlcgsbe/jpa/domain/LCG_Player_Data.java
@@ -9,6 +9,9 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import riot.lcgs.riotlcgsbe.web.dto.object.Player;
+import riot.lcgs.riotlcgsbe.web.dto.object.RankData;
+
+import static riot.lcgs.riotlcgsbe.util.ValidationTool.*;
 
 @NoArgsConstructor
 @Entity
@@ -45,45 +48,72 @@ public class LCG_Player_Data {
     private int lcgSummonerIcon;
 
     @NotNull
-    @Column(name = "lcg_rank_play")
-    private String lcgRankPlay;
-
-    @NotNull
-    @Column(name = "lcg_rank_tier")
-    private String lcgRankTier;
+    @Column(name = "lcg_rank_win")
+    private int lcgRankWin;
 
     @NotNull
     @Column(name = "lcg_rank_point")
     private int lcgRankPoint;
 
     @NotNull
-    @Column(name = "lcg_rank_win")
-    private int lcgRankWin;
+    @Column(name = "lcg_present_tier")
+    private String lcgPresentTier;
 
     @NotNull
-    @Column(name = "lcg_rank_fail")
-    private int lcgRankFail;
+    @Column(name = "lcg_present_division")
+    private String lcgPresentDivision;
+
+    @NotNull
+    @Column(name = "lcg_present_high_tier")
+    private String lcgPresentHighTier;
+
+    @NotNull
+    @Column(name = "lcg_present_high_division")
+    private String lcgPresentHighDivision;
+
+    @NotNull
+    @Column(name = "lcg_previous_tier")
+    private String lcgPreviousTier;
+
+    @NotNull
+    @Column(name = "lcg_previous_division")
+    private String lcgPreviousDivision;
+
+    @NotNull
+    @Column(name = "lcg_previous_high_tier")
+    private String lcgPreviousHighTier;
+
+    @NotNull
+    @Column(name = "lcg_previous_high_division")
+    private String lcgPreviousHighDivision;
 
 
-    public LCG_Player_Data playerDataUpdate(Player playerData) {
-        this.lcgSummonerNickname = playerData.getGameName() + "#" + playerData.getTagLine();
-        this.lcgSummonerId = playerData.getSummonerId();
-        this.lcgSummonerName = playerData.getGameName();
-        this.lcgSummonerTag = playerData.getTagLine();
-        this.lcgSummonerIcon = playerData.getProfileIcon();
-        this.lcgRankPlay = "";
-        this.lcgRankTier = "";
-        this.lcgRankPoint = 0;
-        this.lcgRankWin = 0;
-        this.lcgRankFail = 0;
+    public LCG_Player_Data playerDataUpdate(Player playerData, RankData rankData) {
+        this.lcgSummonerNickname = validationChkString(playerData.getGameName()) + "#" + validationChkString(playerData.getTagLine());
+        this.lcgSummonerId = validationChkLong(playerData.getSummonerId());
+        this.lcgSummonerName = validationChkString(playerData.getGameName());
+        this.lcgSummonerTag = validationChkString(playerData.getTagLine());
+        this.lcgSummonerIcon = validationChkInteger(playerData.getProfileIcon());
+        this.lcgRankWin = validationChkInteger(rankData.getWins());
+        this.lcgRankPoint = validationChkInteger(rankData.getPoints());
+        this.lcgPresentTier = validationChkString(rankData.getPresentTier());
+        this.lcgPresentDivision = validationChkString(rankData.getPresentDivision());
+        this.lcgPresentHighTier = validationChkString(rankData.getPresentHighestTier());
+        this.lcgPresentHighDivision = validationChkString(rankData.getPresentHighestDivision());
+        this.lcgPreviousTier = validationChkString(rankData.getPreviousTier());
+        this.lcgPreviousDivision = validationChkString(rankData.getPreviousDivision());
+        this.lcgPreviousHighTier = validationChkString(rankData.getPreviousHighestTier());
+        this.lcgPreviousHighDivision = validationChkString(rankData.getPreviousHighestDivision());
         return this;
     }
 
     @Builder
     public LCG_Player_Data(String lcgSummonerPuuid, String lcgPlayer, String lcgSummonerNickname,
                            Long lcgSummonerId, String lcgSummonerName, String lcgSummonerTag,
-                           int lcgSummonerIcon, String lcgRankPlay, String lcgRankTier,
-                           int lcgRankPoint, int lcgRankWin, int lcgRankFail) {
+                           int lcgSummonerIcon, int lcgRankPoint, int lcgRankWin,
+                           String lcgPresentTier, String lcgPresentDivision, String lcgPresentHighTier,
+                           String lcgPresentHighDivision, String lcgPreviousTier, String lcgPreviousDivision,
+                           String lcgPreviousHighTier, String lcgPreviousHighDivision) {
         this.lcgSummonerPuuid = lcgSummonerPuuid;
         this.lcgPlayer = lcgPlayer;
         this.lcgSummonerNickname = lcgSummonerNickname;
@@ -91,10 +121,15 @@ public class LCG_Player_Data {
         this.lcgSummonerName = lcgSummonerName;
         this.lcgSummonerTag = lcgSummonerTag;
         this.lcgSummonerIcon = lcgSummonerIcon;
-        this.lcgRankPlay = lcgRankPlay;
-        this.lcgRankTier = lcgRankTier;
-        this.lcgRankPoint = lcgRankPoint;
         this.lcgRankWin = lcgRankWin;
-        this.lcgRankFail = lcgRankFail;
+        this.lcgRankPoint = lcgRankPoint;
+        this.lcgPresentTier = lcgPresentTier;
+        this.lcgPresentDivision = lcgPresentDivision;
+        this.lcgPresentHighTier = lcgPresentHighTier;
+        this.lcgPresentHighDivision = lcgPresentHighDivision;
+        this.lcgPreviousTier = lcgPreviousTier;
+        this.lcgPreviousDivision = lcgPreviousDivision;
+        this.lcgPreviousHighTier = lcgPreviousHighTier;
+        this.lcgPreviousHighDivision = lcgPreviousHighDivision;
     }
 }
