740207e65ae3fcc17b73b6f758b3f5c721374881
rlawo <rlawo32@naver.com>
2025-02-18 00:31:40 +0900

    [RiotLCGS][BE] <JPA> Player_데이터_작업(Champion, Relative)_20250218 (1)

--- src/main/java/riot/lcgs/riotlcgsbe/jpa/repository/LCG_Player_Data_Repository.java
+++ src/main/java/riot/lcgs/riotlcgsbe/jpa/repository/LCG_Player_Data_Repository.java
@@ -3,6 +3,10 @@ package riot.lcgs.riotlcgsbe.jpa.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import riot.lcgs.riotlcgsbe.jpa.domain.LCG_Player_Data;
 
+import java.util.Optional;
+
 public interface LCG_Player_Data_Repository extends JpaRepository<LCG_Player_Data, String> {
     boolean existsLCG_Player_DataByLcgSummonerPuuid(String puuid);
+
+    Optional<LCG_Player_Data> findByLcgPlayer(String name);
 }
