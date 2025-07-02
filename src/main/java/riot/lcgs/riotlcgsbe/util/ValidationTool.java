1b6f0a87e0cfe04bd6844781311e64dc353ef944
rlawo <rlawo32@naver.com>
2025-07-03 00:44:41 +0900

    [RiotLCGS][BE] <Util> ValidationTool_수정_작업_20250703

--- src/main/java/riot/lcgs/riotlcgsbe/util/ValidationTool.java
+++ src/main/java/riot/lcgs/riotlcgsbe/util/ValidationTool.java
@@ -1,36 +1,37 @@
 package riot.lcgs.riotlcgsbe.util;
 
 public class ValidationTool {
+
   
-    public static String validationChkString(String param) {
-		    if(param == null || param.length() == 0) {
-			    throw new NullPointerException();
-		    } else {
-			    return param;
-		    }
-	  }
+	public static String validationChkString(String param) {
+		if(param == null || param.length() == 0) {
+			throw new NullPointerException();
+		} else {
+			return param;
+		}
+	}
 
     public static Integer validationChkInteger(Integer param) {
-		    if(param == null) {
-			    throw new NullPointerException();
-		    } else {
-			    return param;
-		    }
-	  }
+		if(param == null) {
+			throw new NullPointerException();
+		} else {
+			return param;
+		}
+  	}
   
     public static Long validationChkLong(Long param) {
-		    if(param == null) {
-			    throw new NullPointerException();
-		    } else {
-			    return param;
-		    }
-	  }
+		if(param == null) {
+			throw new NullPointerException();
+		} else {
+			return param;
+		}
+	}
   
     public static Boolean validationChkBoolean(Boolean param) {
-		    if(param == null) {
-			    throw new NullPointerException();
-		    } else {
-			    return param;
-		    }
-	  }
+		if(param == null) {
+			throw new NullPointerException();
+		} else {
+			return param;
+		}
+	}
 }
