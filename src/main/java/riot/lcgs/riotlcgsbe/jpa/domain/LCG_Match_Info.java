2405af680df8a8382fea60fe52151dcbd2fd736b
rlawo <rlawo32@naver.com>
2025-07-27 12:48:22 +0900

    [RiotLCGS][BE] <JPA> LCGMatchInfo_컬럼(capture_verify)_추가_작업_20250727

--- src/main/java/riot/lcgs/riotlcgsbe/jpa/domain/LCG_Match_Info.java
+++ src/main/java/riot/lcgs/riotlcgsbe/jpa/domain/LCG_Match_Info.java
@@ -88,13 +88,17 @@ public class LCG_Match_Info {
     @Column(name = "lcg_ai_summary_verify")
     private String lcgAiSummaryVerify;
 
+    @NotNull
+    @Column(name = "lcg_game_capture_verify")
+    private String lcgGameCaptureVerify;
+
     @Builder
     public LCG_Match_Info(Long lcgGameId, String lcgGameDate, String lcgGameMode,
                           String lcgGameType, int lcgGameDuration, int lcgGameMap,
                           String lcgVerMain, int lcgMaxGold, int lcgMaxCrowd, int lcgMaxDpm,
                           int lcgMaxGpm, int lcgMaxDpg, int lcgMaxDpd, int lcgMaxTpd,
-                          int lcgMaxDamageTotal, int lcgMaxDamageTaken,
-                          String lcgAiSummaryContent, String lcgAiSummaryVerify) {
+                          int lcgMaxDamageTotal, int lcgMaxDamageTaken, String lcgAiSummaryContent,
+                          String lcgAiSummaryVerify, String lcgGameCaptureVerify) {
         this.lcgGameId = lcgGameId;
         this.lcgGameDate = lcgGameDate;
         this.lcgGameMode = lcgGameMode;
@@ -113,5 +117,6 @@ public class LCG_Match_Info {
         this.lcgMaxDamageTaken = lcgMaxDamageTaken;
         this.lcgAiSummaryContent = lcgAiSummaryContent;
         this.lcgAiSummaryVerify = lcgAiSummaryVerify;
+        this.lcgGameCaptureVerify = lcgGameCaptureVerify;
     }
 }
