6beee21167b2236bd15a96e9d7f8e39575e1b858
rlawo <rlawo32@naver.com>
2025-09-20 23:38:06 +0900

    [RiotLCGS][BE] <JPA> lcgGameSet_Search_쿼리_추가_20250920

--- src/main/java/riot/lcgs/riotlcgsbe/jpa/repository/LCG_Match_Main_Repository.java
+++ src/main/java/riot/lcgs/riotlcgsbe/jpa/repository/LCG_Match_Main_Repository.java
@@ -3,6 +3,9 @@ package riot.lcgs.riotlcgsbe.jpa.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import riot.lcgs.riotlcgsbe.jpa.domain.LCG_Match_Main;
 
+import java.util.Optional;
+
 public interface LCG_Match_Main_Repository extends JpaRepository<LCG_Match_Main, Long> {
 
+    Optional<LCG_Match_Main> findLCG_Match_MainByLcgGameSetContaining(String gameSet);
 }
