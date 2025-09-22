91de4d86f29c22fa2b20e69ae3c5264d583fce32
rlawo <rlawo32@naver.com>
2025-09-22 23:49:28 +0900

    [RiotLCGS][BE] <JPA> 게임세트_FIND_쿼리_수정_20250922

--- src/main/java/riot/lcgs/riotlcgsbe/jpa/repository/LCG_Match_Main_Repository.java
+++ src/main/java/riot/lcgs/riotlcgsbe/jpa/repository/LCG_Match_Main_Repository.java
@@ -3,9 +3,12 @@ package riot.lcgs.riotlcgsbe.jpa.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import riot.lcgs.riotlcgsbe.jpa.domain.LCG_Match_Main;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface LCG_Match_Main_Repository extends JpaRepository<LCG_Match_Main, Long> {
 
-    Optional<LCG_Match_Main> findLCG_Match_MainByLcgGameSetContaining(String gameSet);
+    Optional<LCG_Match_Main> findTopByLcgGameSetContainingOrderByRowNumDesc(String gameSet);
+
+    List<LCG_Match_Main> findByLcgGameId(Long gameId);
 }
