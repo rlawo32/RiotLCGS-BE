d07027c61f2f6f3e6feb0ce270426a6b91bea175
rlawo <rlawo32@naver.com>
2026-03-30 01:18:37 +0900

    [RiotLCGS][BE] <Service> 성능개선_작업_20250330 (1)

--- src/main/java/riot/lcgs/riotlcgsbe/jpa/repository/LCG_Player_Data_Repository.java
+++ src/main/java/riot/lcgs/riotlcgsbe/jpa/repository/LCG_Player_Data_Repository.java
@@ -3,10 +3,13 @@ package riot.lcgs.riotlcgsbe.jpa.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import riot.lcgs.riotlcgsbe.jpa.domain.LCG_Player_Data;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface LCG_Player_Data_Repository extends JpaRepository<LCG_Player_Data, String>, LCG_Player_Data_RepositoryCustom {
     boolean existsLCG_Player_DataByLcgSummonerPuuid(String puuid);
 
     Optional<LCG_Player_Data> findByLcgPlayer(String name);
+
+    List<LCG_Player_Data> findAllByLcgPlayerIn(List<String> names);
 }
--- src/main/java/riot/lcgs/riotlcgsbe/service/MainService.java
+++ src/main/java/riot/lcgs/riotlcgsbe/service/MainService.java
@@ -1,7 +1,6 @@
 package riot.lcgs.riotlcgsbe.service;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import riot.lcgs.riotlcgsbe.jpa.domain.LCG_Match_Main;
@@ -9,6 +8,7 @@ import riot.lcgs.riotlcgsbe.jpa.domain.LCG_Player_Data;
 import riot.lcgs.riotlcgsbe.jpa.domain.TEST;
 import riot.lcgs.riotlcgsbe.jpa.repository.LCG_Match_Info_Repository;
 import riot.lcgs.riotlcgsbe.jpa.repository.LCG_Match_Main_Repository;
+import riot.lcgs.riotlcgsbe.jpa.repository.LCG_Player_Data_Repository;
 import riot.lcgs.riotlcgsbe.jpa.repository.TEST_Repository;
 import riot.lcgs.riotlcgsbe.util.ExtractionTool;
 import riot.lcgs.riotlcgsbe.web.dto.CommonResponseDto;
@@ -21,6 +21,8 @@ import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import static riot.lcgs.riotlcgsbe.service.HttpService.*;
 import static riot.lcgs.riotlcgsbe.util.AccountCheckTool.playerAccountChk;
@@ -37,54 +39,71 @@ public class MainService {
 
     private final LCG_Match_Info_Repository lcgMatchInfoRepository;
     private final LCG_Match_Main_Repository lcgMatchMainRepository;
+    private final LCG_Player_Data_Repository lcgPlayerDataRepository;
     private final TEST_Repository testRepository;
 
+    @Transactional
     public CommonResponseDto<?> LolCustomGameDataSave(CustomGameRequestDto requestDto) {
-
         GameData gameData = requestDto.getGameData();
         List<TeamData> teamData = requestDto.getTeamData();
 
-        String checkGameData = validationService.ValidationCheckGameData(gameData).getMessage();
+        // 1. 데이터 검증
+        String validationMsg = validationService.ValidationCheckGameData(gameData).getMessage();
         //String checkTeamData = validationService.ValidationCheckTeamData(teamData).getMessage();
-
-        if(checkGameData.equals("Success")) {
-
-            Long gameId = gameData.getGameId();
-
-            gameData = (GameData) playerAccountChk(gameData, teamData).get("gameData");
-            teamData = (List<TeamData>) playerAccountChk(gameData, teamData).get("teamData");
-
-            // TeamId => 100 : Blue , 200 : Red
-            boolean duplicationCheck = lcgMatchInfoRepository.existsLCG_Match_InfoByLcgGameId(gameId);
-
-            if(!duplicationCheck) {
-                if(DataDragonAPIVersion().isResult()) {
-                    Map<String, String> version = DataDragonAPIVersion().getData();
-                    ExtractionTool.jsonChampion = DataDragonAPIChampion().getData();
-                    ExtractionTool.jsonPerk = DataDragonAPIPerk().getData();
-
-                    playerService.LCGPlayerRelativeSave(gameData, teamData);
-                    playerService.LCGPlayerChampionSave(gameData);
-                    playerService.LCGPlayerStatisticsSave(gameData);
-                    playerService.LCGPlayerPositionSave(gameData, teamData);
-                    matchService.LCGMatchInfoSave(gameId, gameData, version);
-                    matchService.LCGMatchEtcSave(version);
-                    matchService.LCGTeamLogSave(gameId, gameData, version);
-                    matchService.LCGMatchMainSave(gameId, gameData, teamData);
-                    matchService.LCGMatchSubSave(gameId, gameData);
-                    matchService.LCGMatchTeamSave(gameId, gameData);
-                    playerService.LCGPlayerRankingSave();
-
-                    return CommonResponseDto.setSuccess("저장 완료", "Success");
-                } else {
-                    return CommonResponseDto.setFailed("통신 실패");
-                }
-            } else {
-                return CommonResponseDto.setFailed("중복 저장");
-            }
-        } else {
-            return CommonResponseDto.setFailed("검증 오류 발생");
+        if (!"Success".equals(validationMsg)) {
+            return CommonResponseDto.setFailed("검증 오류: " + validationMsg);
         }
+
+        // 2. 플레이어 체크
+        List<String> nameList = teamData.stream()
+                .map(TeamData::getName)
+                .collect(Collectors.toList());
+        List<LCG_Player_Data> existingPlayers =lcgPlayerDataRepository.findAllByLcgPlayerIn(nameList);
+        if(existingPlayers.size() != nameList.size()) {
+            Set<String> existingNames = existingPlayers.stream()
+                    .map(LCG_Player_Data::getLcgPlayer)
+                    .collect(Collectors.toSet());
+            List<String> missingNames = nameList.stream()
+                    .filter(name -> !existingNames.contains(name))
+                    .collect(Collectors.toList());
+            return CommonResponseDto.setFailed("존재하지 않는 플레이어: " + String.join(", ", missingNames));
+        }
+
+        // 3. 중복 체크
+        Long gameId = gameData.getGameId();
+        if(lcgMatchInfoRepository.existsLCG_Match_InfoByLcgGameId(gameId)) {
+            return CommonResponseDto.setFailed("중복 저장");
+        }
+
+        // 4. 외부 API 및 버전 체크
+        CommonResponseDto<Map<String, String>> versionResult = DataDragonAPIVersion();
+        CommonResponseDto<String> championResult = DataDragonAPIChampion();
+        CommonResponseDto<List<Perk>> perkResult = DataDragonAPIPerk();
+        if(!versionResult.isResult() || !championResult.isResult() || !perkResult.isResult()) {
+            return CommonResponseDto.setFailed("통신 실패");
+        }
+
+        gameData = (GameData) playerAccountChk(gameData, teamData).get("gameData");
+        teamData = (List<TeamData>) playerAccountChk(gameData, teamData).get("teamData");
+
+        Map<String, String> version = versionResult.getData();
+        ExtractionTool.jsonChampion = championResult.getData();
+        ExtractionTool.jsonPerk = perkResult.getData();
+
+        playerService.LCGPlayerRelativeSave(gameData, teamData);
+        playerService.LCGPlayerChampionSave(gameData);
+        playerService.LCGPlayerStatisticsSave(gameData);
+        playerService.LCGPlayerPositionSave(gameData, teamData);
+        matchService.LCGMatchInfoSave(gameId, gameData, version);
+        matchService.LCGMatchEtcSave(version);
+        matchService.LCGTeamLogSave(gameId, gameData, version);
+        matchService.LCGMatchMainSave(gameId, gameData, teamData);
+        matchService.LCGMatchSubSave(gameId, gameData);
+        matchService.LCGMatchTeamSave(gameId, gameData);
+        playerService.LCGPlayerWinningStreakUpdate(gameData, teamData);
+        playerService.LCGPlayerRankingSave();
+
+        return CommonResponseDto.setSuccess("저장 완료", "Success");
     }
     @Transactional
     public CommonResponseDto<?> LCGCustomGamePlayerSave(PlayerDataRequestDto requestDto) {
