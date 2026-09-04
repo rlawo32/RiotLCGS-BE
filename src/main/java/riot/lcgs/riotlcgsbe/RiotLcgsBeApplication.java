6463b9a576a68ff75b3bdde2d22263548d23e1ae
rlawo <rlawo32@naver.com>
2026-09-04 21:43:37 +0900

    [RiotLCGS][BE] <Tool> Scheduler_동작_추가_20260904

--- src/main/java/riot/lcgs/riotlcgsbe/RiotLcgsBeApplication.java
+++ src/main/java/riot/lcgs/riotlcgsbe/RiotLcgsBeApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 //@SpringBootApplication(scanBasePackages = {"riot.lcgs.riotlcgsbe.jpa.repository"} ,exclude={ JpaRepositoriesAutoConfiguration.class, DataSourceAutoConfiguration.class }) // DB 연결 확인 무시
 @SpringBootApplication
+@EnableScheduling
 public class RiotLcgsBeApplication {
 
 	public static void main(String[] args) {
